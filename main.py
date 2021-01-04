d99ac1182c0e16be8fc4281658f5ac8a95d047f8
rox <cernatmonicaroxana@gmail.com>
2021-01-04 21:07:36 +0200

    Added indicator for supported funct

--- main.py
+++ main.py
@@ -2,6 +2,10 @@ from lexer import Lexer
 from parse import Parser
 from interpreter import Interpreter
 
+print("""
+                        Available functions:
+    +, -, *, / , ^, log (base 10), ln (base e), rad, sin, cos, tg, ctg
+    """)
 while True:
     try:
         expr = input("Enter an expression: \n")
